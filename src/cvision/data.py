f2a194a1ca89aa26e0f9b5bdcc59c5ef13fc1469
Piotr Swiecik <piotr.swiecik@gmail.com>
2024-10-15 11:38:41 +0200

    add multithreaded data downloader

--- src/cvision/data.py
+++ src/cvision/data.py
@@ -1,11 +1,15 @@
 """Custom dataset definitions."""
 
+import requests
 import os
 from pathlib import Path
+import threading
+import time
 from typing import Any
 import zipfile
 
 from PIL import Image
+from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -134,3 +138,95 @@ def get_fruits(
         "val_dataset": ds_val,
         "test_dataset": ds_test
     }
+
+
+# TODO: test
+def threaded_download(remote: str, localdir: str, threads: int = 1):
+    """x"""
+    lpath: Path | None = None
+    try:
+        lpath = Path(localdir)
+    except:
+        raise Exception(f"Local path {localdir} is invalid.")
+    
+    if not lpath.exists():
+        os.makedirs(lpath.absolute())
+
+    # kaggle download links typically don't support HEAD requests
+    # so we use a workaround here
+    preflight = requests.get(remote, headers={"Range": "bytes=0-0"})
+    if "Content-Type" not in preflight.headers:
+        raise Exception(f"Invalid header - Content-Type not specified.")
+    ctype = preflight.headers.get("Content-Type")
+    if ctype != "application/zip":
+        raise Exception(f"Content type {ctype} is currently not supported.")
+    if "Content-Range" not in preflight.headers:
+        raise Exception(f"Remote error - unable to get content length.")
+    length_header = preflight.headers["Content-Range"] # bytes 0-0/n
+    if not "bytes" in length_header or not "/" in length_header:
+        raise Exception(f"Invalid preflight header: {length_header}")
+    length = length_header.split("/")[-1]
+    try:
+        length = int(length)
+    except:
+        raise Exception(f"Content length {length} is invalid.")
+
+    # create byte pointer table
+    part_size = length // threads
+    parts = [(i * part_size, (i + 1) * part_size - 1) for i in range(threads)]
+    parts[-1] = (parts[-1][0], length)
+
+    progress = tqdm(total=length, unit="B", unit_scale=True, desc="Downloading dataset...")
+
+    def _get_chunk(remote: str, localdir: Path, start: int, stop: int, idx: int):
+        """Helper function. Isolate file chunk ands save it to partial file."""
+        headers = {"Range": f"bytes={start}-{stop}"}
+        partial_path = localdir / f"part_{idx}"
+        try:
+            res = requests.get(remote, headers=headers, stream=True)
+            res.raise_for_status()
+            total_downloaded = 0
+            timer_0 = time.time()
+            with open(partial_path, "wb") as part:
+                for chunk in res.iter_content(chunk_size=2**13):
+                    if chunk:
+                        part.write(chunk)
+                        total_downloaded += len(chunk)
+                        progress.update(len(chunk))
+            timer_1 = time.time()
+            speed = total_downloaded / (timer_1 - timer_0)
+        except Exception:
+            raise Exception(f"Download error...")
+        
+    thread_pool = []
+    for idx, (start, end) in enumerate(parts):
+        thread = threading.Thread(
+            group=None,
+            target=_get_chunk,
+            name=f"dl_thread_{idx}",
+            args=(remote, lpath, start, end, idx)
+        )
+        thread_pool.append(thread)
+        thread.start()
+        print(f"Worker thread #{idx} started.")
+
+    for thread in thread_pool:
+        thread.join()
+
+    # recombine file parts
+    out_file = lpath / "archive.zip" # only zip supported for now
+    try:
+        with open(out_file, "wb") as f, tqdm(total=length, unit="B", unit_scale=True, desc="Merging...") as pb:
+            for idx in range(threads):
+                partial_path = lpath / f"part_{idx}"
+                with open(partial_path, "rb") as part:
+                    while True:
+                        data = part.read(1024*1024)
+                        if not data:
+                            break
+                        f.write(data)
+                        pb.update(len(data))
+                os.remove(partial_path)
+        print(f"Done!")
+    except:
+        raise Exception(f"Error while merging partial files!")
